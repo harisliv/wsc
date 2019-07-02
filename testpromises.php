411b9a0ca0e0a723a47857b8bb5a16894e544c04
harisliv <livieratos.theoxaris@gmail.com>
2019-07-02 23:22:30 +0300

    promises

--- testpromises.php
+++ testpromises.php
@@ -23,16 +23,12 @@
       $client = new GuzzleHttp\Client(['base_uri' => 'http://localhost/shedulerapi/controller/']);
       $header = ['headers' => ['Authorization' => $_SESSION["authtoken"]]];
       $room_avail_header = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' => ['id_acadsem' => $_SESSION["id_acadsem"],'available' => 'Y']];
-      $room_header_1 = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' => ['id' => $room_avail_array[$x]->id_room ]];
-      $timeslot_header_1 = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' =>['id'=>$room_avail_array[$x]->id_ts ]];
-      $room_header_2 = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' => ['id' => $scheduler_array[$x]->id_room ]];
-      $timeslot_header_2 = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' =>['id'=>$scheduler_array[$x]->id_ts ]];
-      $course_header = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' => ['id' => $scheduler_array[$x]->id_course]];
       // Initiate each request but do not block
       $promises = [
           'course' => $client->getAsync('course.php', $header),
           'room_avail'  => $client->getAsync('room_avail.php', $room_avail_header),
-          'scheduler'  => $client->getAsync('scheduler.php', $header)
+          'scheduler'  => $client->getAsync('scheduler.php', $header),
+          'professor'  => $client->getAsync('professor.php', $header)
       ];
 
       // Wait on all of the requests to complete. Throws a ConnectException
@@ -50,7 +46,7 @@
       $json = json_decode($string);
       $room_avail_array = $json->data->rooms_avail;
       $room_avail_rows = $json->data->rows_returned;
-      echo "<br> id room: " . $room_avail_array[0]->id_room;
+      //echo "<br> id room: " . $room_avail_array[0]->id_room;
 
       //print_r($room_avail_array);
       //echo $room_avail_rows;
@@ -65,66 +61,104 @@
       $scheduler_array = $json->data->schedulers;
       $scheduler_rows = $json->data->rows_returned;
 
+
+      $body = $results['course']['value']->getBody();
+      $string = $body->getContents();
+      $json = json_decode($string);
+      $course_list_array = $json->data->courses;
+      $course_rows = $json->data->rows_returned;
+
+
+      $body = $results['professor']['value']->getBody();
+      $string = $body->getContents();
+      $json = json_decode($string);
+      $professor_array = $json->data->professors;
+      $professor_rows = $json->data->rows_returned;
+
       //print_r($scheduler_array);
       //echo $scheduler_rows;
 
+      $room_header_1 = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' => ['id' => $room_avail_array[$x]->id_room ]];
+      $timeslot_header_1 = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' =>['id'=>$room_avail_array[$x]->id_ts ]];
 
       for ($x = 0; $x < $room_avail_rows; $x++) {
         $promises = [
             'room_1'   => $client->getAsync('room.php', $room_header_1),
-            'timeslot_1'  => $client->getAsync('timeslot.php', $timeslot_header_1)
+            'timeslot_1'  => $client->getAsync('timeslot.php', ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' =>['id'=>$room_avail_array[$x]->id_ts ]])
         ];
+
+        $results = Promise\unwrap($promises);
+
+        // Wait for the requests to complete, even if some of them fail
+        $results = Promise\settle($promises)->wait();
+
+
+        $body = $results['room_1']['value']->getBody();
+        $string = $body->getContents();
+        $room_array[$x] = json_decode($string);
+        //print_r($room_array);
+
+        $body = $results['timeslot_1']['value']->getBody();
+        $string = $body->getContents();
+        $timeslot_array[$x] = json_decode($string);
       }
 
-      $results = Promise\unwrap($promises);
 
-      // Wait for the requests to complete, even if some of them fail
-      $results = Promise\settle($promises)->wait();
+      //print_r($timeslot_array);
 
+      //echo "<br>ts " . $timeslot_array->data->timeslots[2]->start_time;
 
-      $body = $results['room_1']['value']->getBody();
-      $string = $body->getContents();
-      $room_array = json_decode($string);
-
-      //print_r($room_array);
-
-      $body = $results['timeslot_1']['value']->getBody();
-      $string = $body->getContents();
-      $timeslot_array = json_decode($string);
-      print_r($timeslot_array);
-
-      echo "<br>ts " . $timeslot_array->data->timeslots[2]->start_time;
+      $room_header_2 = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' => ['id' => $scheduler_array[$x]->id_room ]];
+      $timeslot_header_2 = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' =>['id' => $scheduler_array[$x]->id_ts]];
+      $course_header = ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' => ['id' => $scheduler_array[$x]->id_course]];
+      //print_r($room_header_2);
 
 
       for ($x = 0; $x < $scheduler_rows; $x++) {
         $promises = [
             'room_2'   => $client->getAsync('room.php', $room_header_2),
-            'timeslot_2'  => $client->getAsync('timeslot.php', $timeslot_header_2),
+            'timeslot_2'  => $client->getAsync('timeslot.php', ['headers' => ['Authorization' => $_SESSION["authtoken"]],'query' =>['id'=>$scheduler_array[$x]->id_ts ]]),
             'course_2'  => $client->getAsync('course.php', $course_header)
         ];
+
+        $results = Promise\unwrap($promises);
+
+        // Wait for the requests to complete, even if some of them fail
+        $results = Promise\settle($promises)->wait();
+
+        // You can access each result using the key provided to the unwrap
+        // function.
+
+
+        $body = $results['room_2']['value']->getBody();
+        $string = $body->getContents();
+        $room_array_sch[$x] = json_decode($string);
+        //print_r($room_array_sch);
+
+        $body = $results['timeslot_2']['value']->getBody();
+        $string = $body->getContents();
+        $timeslot_array_sch[$x] = json_decode($string);
+        //print_r($timeslot_array_sch);
+
+        $body = $results['course_2']['value']->getBody();
+        $string = $body->getContents();
+        $course_array[$x] = json_decode($string);
+
       }
 
-      $results = Promise\unwrap($promises);
 
-      // Wait for the requests to complete, even if some of them fail
-      $results = Promise\settle($promises)->wait();
-
-      // You can access each result using the key provided to the unwrap
-      // function.
+      //print_r($course_array);
+      //echo $scheduler_array[0]->id_course;
+      //echo $course_array->data->courses[0]->name;
 
 
-      $body = $results['room_2']['value']->getBody();
-      $string = $body->getContents();
-      $room_array_sch = json_decode($string);
-      print_r($room_array_sch);
+      if($scheduler_rows > $room_avail_rows){
+        $loop = $scheduler_rows;
+      }
+      else {
+        $loop = $room_avail_rows;
+      }
 
-      $body = $results['timeslot_2']['value']->getBody();
-      $string = $body->getContents();
-      $timeslot_array_sch = json_decode($string);
-
-      $body = $results['course_2']['value']->getBody();
-      $string = $body->getContents();
-      $course_array = json_decode($string);
       ?>
 
       <pre> <?php //print_r($json->data->rooms[0]->id); ?> </pre>
@@ -147,8 +181,8 @@
             <?php for ($y = 0; $y <= 4; $y++) { ?>
             <td><?php for ($x = 0; $x < $loop; $x++) {
               if($timeslot_array[$x]->data->timeslots[0]->start_time == $st && $weekdb[$y] === $timeslot_array[$x]->data->timeslots[0]->day) {
-                //echo $timeslot_array[$x]->data->timeslots[0]->start_time . ":00";
-                //echo str_replace($weekdb[$y], $weekgk[$y], $timeslot_array[$x]->data->timeslots[0]->day);
+                //echo $timeslot_array->data->timeslots[$x]->start_time . ":00";
+                //echo str_replace($weekdb[$y], $weekgk[$y], $timeslot_array->data->timeslots[$x]->day);
                 ?><div class="form-check">
                 <input class="form-check-input" type="radio" name="testtableradio[<?php echo $x;?>]" value="<?php echo $room_avail_array[$x]->id; ?>" >
                 <?php echo $room_array[$x]->data->rooms[0]->lektiko_room; ?>
@@ -156,10 +190,11 @@
                 <?php
               }
                 elseif($timeslot_array_sch[$x]->data->timeslots[0]->start_time == $st && $weekdb[$y] === $timeslot_array_sch[$x]->data->timeslots[0]->day) {
-                  //echo $timeslot_array[$x]->data->timeslots[0]->start_time . ":00";
                   //echo str_replace($weekdb[$y], $weekgk[$y], $timeslot_array[$x]->data->timeslots[0]->day);
-                  echo $room_array_sch[$x]->data->rooms[0]->lektiko_room;
-                  echo $course_array[$x]->data->courses[0]->name;
+                  echo $room_array_sch[$x]->data->rooms[0]->lektiko_room . "<br>";
+                  echo $course_array[$x]->data->courses[0]->name . "<br>";
+                  echo $scheduler_array[$x]->type_division . "<br>";
+                  echo $scheduler_array[$x]->division_str;
                 }
             }
             ?>
@@ -174,9 +209,9 @@
          <div class='form-group'>
              <label>Course</label>
              <select name='id_course'>
-               <?php for ($x = 0; $x < $json4->data->rows_returned; $x++) { ?>
-               <option value="<?php print_r($json4->data->courses[$x]->id); ?>">
-                 <?php print_r($json4->data->courses[$x]->name); ?>
+               <?php for ($x = 0; $x < $course_rows; $x++) { ?>
+               <option value="<?php print_r($course_list_array[$x]->id); ?>">
+                 <?php print_r($course_list_array[$x]->name); ?>
                </option>
              <?php } ?>
              </select>
@@ -190,9 +225,9 @@
            <div class='form-group'>
                <label>Καθηγητής</label>
                <select name='id_prof'>
-                 <?php for ($x = 0; $x < $json5->data->rows_returned; $x++) { ?>
-                 <option value="<?php print_r($json5->data->professors[$x]->id); ?>">
-                   <?php print_r($json5->data->professors[$x]->fullname); ?>
+                 <?php for ($x = 0; $x < $professor_rows; $x++) { ?>
+                 <option value="<?php print_r($professor_array[$x]->id); ?>">
+                   <?php print_r($professor_array[$x]->fullname); ?>
                  </option>
                <?php } ?>
                </select>
